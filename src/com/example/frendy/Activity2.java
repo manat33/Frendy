8f0cf339441d3a300294614564afedc65a25c24b
manat33 <chris.peterson@cyberfamily.us>
2015-02-07 12:21:28 -0500

    VTHacksII

---
+++ src/com/example/frendy/Activity2.java
@@ -0,0 +1,36 @@
+package com.example.frendy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.Button;
+
+public class Activity2 extends Activity implements View.OnClickListener {
+    /**
+     * Called when the activity is first created.
+     */
+    private Button coffeeButton;
+    private String activity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        coffeeButton = (Button)findViewById(R.id.coffee);
+        coffeeButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        //respond to click
+        if(v.getId()==coffeeButton.getId()){
+            //the button was clicked
+            activity = "coffee";
+            Intent myIntent = new Intent(this, Activity2.class);
+
+        }
+    }
+
+
+
+}
---
+++ src/com/example/frendy/MyActivity.java
@@ -0,0 +1,40 @@
+package com.example.frendy;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.view.*;
+
+public class MyActivity extends Activity implements View.OnClickListener {
+    /**
+     * Called when the activity is first created.
+     */
+    private Button coffeeButton;
+    private  int activity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        coffeeButton = (Button)findViewById(R.id.coffee);
+        coffeeButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        //respond to click
+        if (v.getId() == coffeeButton.getId()) {
+            //the button was clicked
+            activity = 1;
+            Intent myIntent = new Intent(this, Activity2.class);
+            startActivityForResult(myIntent, 0);
+
+        }
+    }
+
+
+
+}
